04ad5c32087ec20b5c41fa9805f1de5ec0f9153e
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2023-11-10 17:40:26 +0100

    Header and fundamentals

---
+++ assets/js/mobile-nav.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const mobileNavBtn = document.querySelector('[data-mobile-drop-down-active]');
+    
+    document.addEventListener('click', function(event){
+
+        if(event.target.hasAttribute('data-mobile-nav-btn-active') || event.target.closest('[data-mobile-nav-btn-active]')){
+
+            const btn = event.target.closest('[data-mobile-nav-btn-active]') || event.target;
+            const btnActive = btn.getAttribute('data-mobile-nav-btn-active');
+
+            btn.setAttribute('data-mobile-nav-btn-active', 'false');
+
+            if(btnActive === '' || btnActive === 'false'){
+                btn.setAttribute('data-mobile-nav-btn-active', 'true');
+                mobileNavBtn.setAttribute('data-mobile-drop-down-active', 'true');
+            } else {
+                btn.setAttribute('data-mobile-nav-btn-active', 'false');
+                mobileNavBtn.setAttribute('data-mobile-drop-down-active', 'false');
+            }
+
+        }
+    })
+
+});
